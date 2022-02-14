d6303eb3d78715a5014f72a1a4ea1cb38dc10ae4
Rick Smoke <rsmoke@umich.edu>
2022-02-14 17:53:35 -0500

    tweak sort

--- app/admin/payments.rb
+++ app/admin/payments.rb
@@ -16,8 +16,8 @@ ActiveAdmin.register Payment do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-
-  filter :user, as: :select
+  # filter :user_id, label: "User", as: :select, collection: -> { Application.all.map { |app| [app.display_name, app.user_id]}.uniq.sort}
+  filter :user, as: :select, collection: -> { User.all.order(:email) }
   index do
     selectable_column
     actions
