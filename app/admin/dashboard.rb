706b0213b57b5ea1c3359c2c8147512203c53b22
Rick Smoke <rsmoke@umich.edu>
2022-03-21 12:13:33 -0400

    cleanup dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,13 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
+  
   content title: proc { I18n.t("active_admin.dashboard") } do
+    div do
+      span do
+        link_to('Admin Documentation', 'https://docs.google.com/document/d/1_FS9pUxsBbl7o8tDFY9-15XcwpqBMzsZFhGoJQLMwVg/edit?usp=sharing', :target => "_blank")
+      end
+    end
     if (current_application_settings.opendate + current_application_settings.application_open_period.hours) < Time.now
       if current_application_settings.lottery_result.nil?
         div do
@@ -42,7 +48,7 @@ ActiveAdmin.register_page "Dashboard" do
 
     columns do
       column do
-        panel "#{ApplicationSetting.get_current_app_year} Applicants who accepted their registration offer" do
+        panel "#{ApplicationSetting.get_current_app_year} Applicants who accepted their registration offer (#{Application.application_accepted.count})" do
           table_for Application.application_accepted.order("id desc").limit(110) do
             column("User") { |u| link_to(u.user.email, admin_application_path(u.id)) }
             column("Offer Date") { |od| od.offer_status_date }
@@ -51,18 +57,10 @@ ActiveAdmin.register_page "Dashboard" do
       end
 
       column do
-        panel "Waiting for responses from these #{ApplicationSetting.get_current_app_year} applicants" do
+        panel "Waiting for responses from these #{ApplicationSetting.get_current_app_year} applicants (#{Application.application_offered.count})" do
           table_for Application.application_offered.order("id desc").limit(50) do
             column("User") { |u| link_to(u.user.email, admin_application_path(u.id)) }
             column("Offer Date") { |od| od.offer_status_date }
-          end
-        end
-      end
-
-      column do
-        panel "Resources" do
-          ul do
-            li link_to('Admin Documentation', 'https://docs.google.com/document/d/1_FS9pUxsBbl7o8tDFY9-15XcwpqBMzsZFhGoJQLMwVg/edit?usp=sharing', :target => "_blank")
           end
         end
       end
