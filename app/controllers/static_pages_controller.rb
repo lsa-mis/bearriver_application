01a17182cdcea5908b42ee26e16dd6597839b589
Rick Smoke <rsmoke@umich.edu>
2023-04-02 14:53:52 -0400

    remove authentication requirement for special accts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,4 @@
 class StaticPagesController < ApplicationController
-  # before_action :current_application_open?, only: [:index]
-  before_action :authenticate_user!, only: [ :special_scholarship ]
 
   def index
     redirect_to conference_closed_url unless current_application_open?
@@ -31,7 +29,6 @@ class StaticPagesController < ApplicationController
   end
 
   def special_scholarship
-    redirect_to root_url unless ["scholarship", "special"].include? current_user.payments.current_conference_payments.pluck(:account_type)
   end 
 
 end
