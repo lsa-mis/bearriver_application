afb9c317b5cac730ffdd175beef954a467295ed2
rsmokeUM <rsmoke@umich.edu>
2023-05-23 15:56:13 -0400

    setup mailer class for balance due

--- app/mailers/balance_due_mailer.rb
+++ app/mailers/balance_due_mailer.rb
@@ -3,6 +3,6 @@ class BalanceDueMailer < ApplicationMailer
   def outstanding_balance
     @application = params[:app]
     @current_application_settings = ApplicationSetting.get_current_app_settings
-    mail(to: @application.email, subject: "Your #{@current_application_settings.contest_year} Bear River Writers’ Conference has an outstanding balance.")
+    mail(to: @application.email, subject: "Your #{@current_application_settings.contest_year} Bear River Writers’ Conference payment due notification.")
   end
 end
