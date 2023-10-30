c42b32ab03f7d4863966c6e7b5d326dd51c5754b
rsmokeUM <rsmoke@umich.edu>
2023-10-30 14:59:43 -0400

    config headless

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,4 +75,8 @@ RSpec.configure do |config|
     end
   end
 
+  config.before(:each, type: :system) do
+    driven_by ENV['SHOW_BROWSER'] ? :selenium_chrome : :selenium_chrome_headless
+  end
+
 end
